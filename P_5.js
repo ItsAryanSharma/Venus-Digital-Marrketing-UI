66ccd3678bfa3af24be899e983bdbbdb12d15ba3
ItsAryanSharma <123888689+ItsAryanSharma@users.noreply.github.com>
2023-11-05 19:29:23 +0530

    Fourth Release

---
+++ P_5.js
@@ -0,0 +1,44 @@
+// Active Navbar
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 20) {
+        nav.classList.add("scroll-on");
+    }
+    else {
+        nav.classList.remove("scroll-on");
+    }
+}
+
+//  Nav hide
+let navBar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+navBar.forEach(function (a) {
+    a.addEventListener("click", function (event) {
+        if (event.target.classList.contains("dropdown-toggle")) {
+            return; // Prevent hiding the menu for dropdown toggle clicks
+        }
+        navCollapse.classList.remove("show");
+    })
+})
+
+// Counter design
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration) {
+        let obj = document.getElementById(id),
+            current = start,
+            range = end - start,
+            increment = end > start ? 1 : -1,
+            step = Math.abs(Math.floor(duration / range)),
+            timer = setInterval(() => {
+                current += increment;
+                obj.textContent = current;
+                if (current == end) {
+                    clearInterval(timer);
+                }
+            }, step);
+    }
+    counter("count1", 0, 220, 3400);
+    counter("count2", 0, 120, 3400);
+    counter("count3", 0, 70, 3400);
+    counter("count4", 0, 200, 3400);
+});
